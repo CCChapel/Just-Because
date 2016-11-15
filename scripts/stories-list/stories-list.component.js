703d934a65f9328ad61ace12f6f357011c0ce91b
Eric Rovtar <eric.rovtar@me.com>
2016-11-15 11:22:34 -0500

    Update Sample JSON

--- scripts/stories-list/stories-list.component.js
+++ scripts/stories-list/stories-list.component.js
@@ -1,22 +1,11 @@
 //Define compenent for Story
 angular.module('storiesApp')
        .component('storiesList', {
-           templateUrl: 'scripts/stories-list/stories-list.template.html',
-            controller: function StoriesController() {
-                this.stories = [
-                    {
-                        name: "name 1",
-                        location: "location 1",
-                        story: "Story 1"
-                    }, {
-                        name: "name 2",
-                        location: "location 2",
-                        story: "Story 2"
-                    }, {
-                        name: "name 3",
-                        location: "location 3",
-                        story: "Story 3"
-                    }
-                ];
+            templateUrl: 'scripts/stories-list/stories-list.template.html',
+            controller: function StoriesListController($http) {
+                var self = this;
+                $http.get('scripts/stories-list/stories.json').then(function(response) {
+                    self.stories = response.data;
+                });
             }
        });
