6ad132e60d590910b7e22002f5e1681c68af09b6
Eric Rovtar <eric.rovtar@me.com>
2016-11-15 10:54:56 -0500

    Disbale tagboard

--- scripts/stories-list/stories-list.component.js
+++ scripts/stories-list/stories-list.component.js
@@ -27,3 +27,25 @@ angular.module('storiesApp')
                 ];
             }
        });
+
+// //Define compenent for Story
+// angular.module('storiesApp')
+//        .component('storiesList', {
+//            template:
+//                 '<ul>' +
+//                     '<li ng-repeat="story in $ctrl.stories">' +
+//                         '<div>{{story.name}}</div>' +
+//                         '<div>{{story.location}}</div>' +
+//                         '<div>{{story.story}}</div>' +
+//                     '</li>' +
+//                 '</ul>',
+//             controller: function StoriesController($http) {
+//                     var self = this;
+//                     var url = '/scripts/stories-list/stories.json'; //'https://www.formstack.com/api/v2/form/2530077/submission.json?oauth_token=16559620d4a936952cde88ee1070a6cc';
+
+//                     $http.get(url).then(function(response) {
+//                         console.log(response.data);
+//                         self.stories = response.data;
+//                     });
+//                 }
+//        });
