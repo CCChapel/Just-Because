c993b9c8b1e368fa1ac4c4fe10bb8999c1910d5a
Eric Rovtar <eric.rovtar@me.com>
2016-11-16 13:26:33 -0500

    Clean up old code

--- scripts/stories-list/stories-list.component.js
+++ scripts/stories-list/stories-list.component.js
@@ -1,30 +1,3 @@
-// //Define compenent for Story
-// angular.module('storiesApp')
-//        .component('storiesList', {
-//             templateUrl: 'scripts/stories-list/stories-list.template.html',
-//             controller: function StoriesController() {
-//                 var self = this;
-
-//                 this.stories = [
-//                     {
-//                         name: "name 1",
-//                         location: "location 1",
-//                         story: "Story 1"
-//                     }, {
-//                         name: "name 2",
-//                         location: "location 2",
-//                         story: "Story 2"
-//                     }, {
-//                         name: "name 3",
-//                         location: "location 3",
-//                         story: "Story 3"
-//                     }
-//                 ];
-
-//                 console.log(self);
-//             }
-//        });
-
 //Define compenent for Story
 angular.module('storiesApp')
        .component('storiesList', {
